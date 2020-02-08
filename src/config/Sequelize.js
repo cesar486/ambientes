6673b7fb9b4b2bcc0a5fdf97f072df25ed2ab089
cesar486 <56007115+cesar486@users.noreply.github.com>
2020-02-07 20:18:51 -0500

    segundo comitt

--- src/config/Sequelize.js
+++ src/config/Sequelize.js
@@ -5,8 +5,8 @@ const reserva_model = require('../modelos/Reserva');
 const ambiente_model = require('../modelos/Ambiente');
 const usuario_model = require('../modelos/Usuario');
 
-const conexion = new Sequelize("ambientes", "root", "amigos", {
-  host: 'localhost',
+const conexion = new Sequelize("qj2ioA03vp", "qj2ioA03vp", "kcnNzeFjGv", {
+  host: 'emotemysql.com',
   dialect: 'mysql',
   dialectOptions: {
     useUTC: false,
